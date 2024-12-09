b30fe863765e73adddd4309537f1a2255e55439f
Adem Özay <ozayadem@gmail.com>
2024-12-10 00:50:21 +0300

    feat: adjust app bootstrap

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import './setup';
 
+import { Server } from './application/http/server';
 import { CompositionRoot } from './compositionRoot';
 import { logger } from './logger';
 
 async function main() {
-  CompositionRoot.register();
+  const compositionRoot = CompositionRoot.getInstance();
+  await compositionRoot.bind();
+
+  const { baseContext, controllers } = compositionRoot;
+
+  const httpPort = process.env.HTTP_PORT ? parseInt(process.env.HTTP_PORT) : 8080;
+  const server = Server.create(baseContext, controllers, httpPort);
+  server.bootstrap();
+  await server.start();
+
+  logger.info('🚀 rexx is running');
 }
 
 main().catch((error) => {

5e5730a8bd79c8d3fc7131f16fad12476a05414d
Adem Özay <ozayadem@gmail.com>
2024-12-14 18:51:13 +0300

    feat: add validation error creator from zod error

--- src/application/http/response.ts
+++ src/application/http/response.ts
@@ -1,9 +1,35 @@
+import { z } from 'zod';
+
 export type ErrorResponse = {
   error: {
     message: string;
+    validationErrors?: ValidationError[];
   };
+};
+
+export type ValidationError = {
+  field: string;
+  message: string;
 };
 
 export function createErrorResponse(message: string): ErrorResponse {
   return { error: { message } };
+}
+
+export function createValidationErrorResponse(zodError: z.ZodError): ErrorResponse {
+  const message = 'Invalid request';
+  const error = createErrorResponse(message);
+
+  const validationErrors = zodError.errors.map<ValidationError>((zodIssue) => ({
+    // TODO: find a better way to get the field name
+    field: zodIssue.path.join('.').split('.').pop() ?? '',
+    message: zodIssue.message,
+  }));
+
+  return {
+    error: {
+      ...error.error,
+      validationErrors,
+    },
+  };
 }
